e843dbcdb0c1902f886003713964cb74e86bba56
MMMIIIN <wbsldj7645@naver.com>
2021-10-12 19:37:26 +0900

    bootpay ios setting

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import SwiftyBootpay
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -8,6 +9,14 @@ import Flutter
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+        Bootpay.sharedInstance.appLaunch(application_id: "616422f87b5ba4001d52c1f0")
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
+    override func applicationWillResignActive(_ application: UIApplication) {
+            Bootpay.sharedInstance.sessionActive(active: false)
+        }
+
+        override func applicationDidBecomeActive(_ application: UIApplication) {
+            Bootpay.sharedInstance.sessionActive(active: true)
+        }
 }
