c78d27d77c993b989abfd7327061ed3b43d402ea
MMMIIIN <wbsldj7645@naver.com>
2021-10-18 18:57:31 +0900

    firebase functions deploy

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,33 @@
+// import { RestClient } from "@bootpay/server-rest-client";
+// or
+const RestClient = require("@bootpay/server-rest-client").RestClient;
+
 const functions = require("firebase-functions");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
- exports.helloWorld = functions.https.onRequest((request, response) => {
-   functions.logger.info("Hello logs!", {structuredData: true});
-   response.send("Hello from Firebase!");
- });
+const admin = require("firebase-admin");
+admin.initializeApp();
+
+RestClient.setConfig(
+  "616422f87b5ba4001d52c1f1",
+  "ShSUsfXJBLdwh58Vm2hOdePJXRW821xfP8QRcpNB9sI="
+);
+
+exports.checkPay = functions.https.onRequest((request, response) => {
+  try {
+    RestClient.getAccessToken().then(function (response) {
+      if (response.status === 200) {
+        console.log(response.data.token);
+      }
+    });
+  } catch (e) {
+    console.log(e);
+  }
+});
+
+// // // Create and Deploy Your First Cloud Functions
+// // // https://firebase.google.com/docs/functions/write-firebase-functions
+// //
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", { structuredData: true });
+//   response.send("Hello from Firebase!");
+// });
